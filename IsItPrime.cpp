10095ea136c34eccefe5e68cf54513b0ca42d624
1emonwastaken <97442179+1emonwastaken@users.noreply.github.com>
2022-02-28 20:10:07 +0530

    Create IsItPrime.cpp

---
+++ IsItPrime.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, t, factors = 0;
+    cin >> t;
+    for(i = 1; i <= t; i++)
+    {
+        if(t % i == 0)
+        {
+            factors++;
+        }
+    }
+    if(factors == 2)
+    {
+        cout << "This number is prime.";
+    }
+    else
+    {
+        cout << "This number isn't prime.";
+    }
+}
