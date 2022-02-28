73ce4048d147f95dea8ab875169e358ba303fe53
1emonwastaken <97442179+1emonwastaken@users.noreply.github.com>
2022-02-28 19:13:29 +0530

    Create PerfectNumber.cpp

---
+++ PerfectNumber.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t = 99999999;
+    //cin >> t;
+  cout << "\n\n Find the perfect numbers between 1 and " << t << ":\n";
+  cout << "------------------------------------------------\n";
+  int i = 1, u = 1, sum = 0;
+  cout << "\n The perfect numbers between 1 to " << t << " are: \n";
+  while (i <= t)
+  {
+    while (u <= t)
+    {
+      if (u < i)
+      {
+        if (i % u == 0)
+          sum = sum + u;
+      }
+      u++;
+    }
+    if (sum == i) {
+      cout << i << "  " << "\n";
+    }
+    i++;
+    u = 1;
+    sum = 0;
+  }
+  return 0;
+}
